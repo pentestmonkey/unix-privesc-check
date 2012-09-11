6ca8b81ba83afff519bf781394f9c1a9a8718c39
timb.machine@gmail.com <timb.machine@gmail.com@245aaf92-7693-11de-b058-93adefbabda0>
2012-09-11 06:54:43 +0000

    Tagging version 2.0

---
+++ tags/r2_0/upc.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# $Revision$
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# (c) Tim Brown, 2012
+# <mailto:timb@nth-dimension.org.uk>
+# <http://www.nth-dimension.org.uk/> / <http://www.machine.org.uk/>
+
+# TODO tidy up config names a little
+filecachefilename="files_cache.tmp"
+filecacherootpath="/"
+filecacheignoresymlinksflag=1
+
+for checkfilename in lib/checks/*
+do
+	if [ -x "${checkfilename}" ]
+	then
+		. "${checkfilename}"
+		`basename "${checkfilename}"`_init
+		`basename "${checkfilename}"`_main
+		`basename "${checkfilename}"`_fini
+	fi
+done
